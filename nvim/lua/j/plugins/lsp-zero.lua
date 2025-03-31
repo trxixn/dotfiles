c1187698bc50dcf1e432cebcc5a4673a5263d4f7
trxixn <sfarghiutraian999@gmail.com>
2025-04-01 00:33:37 +0300

    Initial commit

---
+++ nvim/lua/j/plugins/lsp-zero.lua
@@ -0,0 +1,146 @@
+return {
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    lazy = true,
+    config = false,
+    init = function()
+      -- Disable automatic setup, we are doing it manually
+      vim.g.lsp_zero_extend_cmp = 0
+      vim.g.lsp_zero_extend_lspconfig = 0
+    end,
+  },
+  {
+    'williamboman/mason.nvim',
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+    config = true,
+  },
+
+  -- snippets
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      'saadparwaiz1/cmp_luasnip',
+      'rafamadriz/friendly-snippets' },
+  },
+
+  -- Autocompletion
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      {'L3MON4D3/LuaSnip'},
+    },
+    config = function()
+      -- Here is where you configure the autocompletion settings.
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_cmp()
+
+      -- And you can configure cmp even more, if you want to.
+      local cmp = require('cmp')
+      local cmp_action = lsp_zero.cmp_action()
+
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        formatting = lsp_zero.cmp_format({details = true}),
+        sources = {
+          {name = 'nvim_lsp'},
+          {name = 'luasnip'},
+          },
+        mapping = cmp.mapping.preset.insert({
+          ['<CR>'] = cmp.mapping.confirm({select = false}),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+        }),
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+      })
+    end
+  },
+
+  -- LSP
+  {
+    'neovim/nvim-lspconfig',
+    cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+    event = {'BufReadPre', 'BufNewFile'},
+    dependencies = {
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'williamboman/mason-lspconfig.nvim'},
+    },
+    config = function()
+      -- This is where all the LSP shenanigans will live
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_lspconfig()
+
+      --- if you want to know more about lsp-zero and mason.nvim
+      --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+      lsp_zero.on_attach(function(client, bufnr)
+        -- see :help lsp-zero-keybindings
+        -- to learn the available actions
+        lsp_zero.default_keymaps({buffer = bufnr})
+      end)
+
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          'ts_ls',  -- Use ts_ls instead of tsserver
+          'pyright',
+          'lua_ls',
+          'html',
+          'cssls',
+          'clangd',
+        },
+        handlers = {
+          -- Default handler for all LSP servers
+          function(server_name)
+            require('lspconfig')[server_name].setup({})
+          end,
+
+          -- Custom handler for Lua language server
+          lua_ls = function()
+            -- (Optional) Configure lua language server for neovim
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+          end,
+
+          -- Custom handler for ts_ls
+          ts_ls = function()
+            require('lspconfig').ts_ls.setup({
+              on_attach = function(client, bufnr)
+                -- Disable formatting if using an external formatter like Prettier
+                client.server_capabilities.documentFormattingProvider = false
+
+                -- Keymaps and additional configuration
+                local opts = { noremap = true, silent = true }
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rf', ':TypescriptRenameFile<CR>', opts)
+              end,
+              settings = {
+                typescript = {
+                  inlayHints = {
+                    includeInlayParameterNameHints = "all",
+                    includeInlayFunctionLikeReturnTypeHints = true,
+                  },
+                },
+                javascript = {
+                  inlayHints = {
+                    includeInlayParameterNameHints = "all",
+                    includeInlayFunctionLikeReturnTypeHints = true,
+                  },
+                },
+              },
+            })
+          end,
+        }
+      })
+    end
+  }
+}
