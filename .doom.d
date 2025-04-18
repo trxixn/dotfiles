22227fe05e8328b24f629dfe7ffdb127d1d6c154
trxixn <sfarghiutraian999@gmail.com>
2025-04-18 11:52:54 +0300

    Synced

--- .doom.d
+++
@@ -1,1 +0,0 @@
-/Users/traiansfarghiu/.doom.d
---
+++ nvim/init.lua
@@ -0,0 +1,100 @@
+vim.g.mapleader = ' '
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("j.plugins")
+
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme rose-pine]])
+
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
+
+-- nvim-tree binds 
+vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
+require("lspconfig").ts_ls.setup({})
+
+-- nvim-ale config
+vim.cmd [[
+  let g:ale_linters = {
+      \ 'typescript': ['tsserver'],
+      \ 'typescriptreact': ['tsserver'],
+      \ }
+  let g:ale_fixers = {
+      \ 'typescript': ['prettier'],
+      \ 'typescriptreact': ['prettier'],
+      \ }
+  let g:ale_fix_on_save = 1
+]]
+
+-- telescope binds
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- highlight clear
+vim.keymap.set('n', '<leader>n', ':noh<CR>', { silent = true })
+
+
+vim.g.python3_host_prog = '/usr/bin/python3'
+
+
+
+-- Default: 4 spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Filetype-specific settings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "json", "html", "css", "yaml", "lua" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "python" },
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "go" },
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end,
+})
